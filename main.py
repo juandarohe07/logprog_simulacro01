2dd2b2e8162a512983df37fa974bef233aed8e8b
Juan David Rodríguez <juand.rodriguezh@upb.edu.co>
2025-02-12 09:18:24 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+print(' Ligerini: Comprometidos con el medio ambiente \n')
+print ('Reducción huella de cabono\n')
+print ('Programa que calcula promedio de contaminación\n')
+recorrido=int(input('Ingrese los kilometros recorrido : \n'))
+print ('La distancia recorrida fue de: \n',recorrido)
+#Condicional
+if recorrido>=5:
+  print('El dato es válido')
+else:
+  print ('El dato es inválido')
+#Condicional
+if recorrido>=5:
+  print ('Gramos de dioxido de carbono emitidos')
+  print(recorrido*43)
